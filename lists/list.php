c2c58c62f0785573af7e21767e0b58bbac3e3712
Oleh Zmyi <zmyy.oleg@gmail.com>
2021-03-01 17:25:20 +0200

    logout

--- lists/list.php
+++ lists/list.php
@@ -60,7 +60,7 @@ try {
     <?php
     if ($_SESSION['user'][0]['name']) : ?>
         <p>Hi, <?= $_SESSION['user'][0]['name']; ?></p>
-        <p><a href="logout.php">sign out</a></p>
+        <p><a href="../logout.php">sign out</a></p>
     <?php else: ?>
         <p>Hi, Guest</p>
     <?php endif; ?>
